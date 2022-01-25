b3bb527d470970b06274a664f9205b55b60ae981
JustinMcLain1 <70154173+JustinMcLain1@users.noreply.github.com>
2022-01-25 14:35:19 -0600

    Doc Box

---
+++ assignment00.cc
@@ -0,0 +1,8 @@
+/* Justin McLain
+   Z1910087
+   Csci480 section PE1
+
+   i certify that this is my own work and where appropriate an extension
+   of teh starter code provided from the assignment
+
+*/
